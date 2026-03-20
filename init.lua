19cacdec254d86a098c70857a8b0d42aac8f9276
Ye Lin Aung <me@yelinaung.com>
2026-03-20 22:51:55 +0800

    tree-sitter: add more languages

--- init.lua
+++ init.lua
@@ -1000,7 +1000,7 @@ require('lazy').setup({
     branch = 'main',
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter-intro`
     config = function()
-      local parsers = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'go' }
+      local parsers = { 'bash', 'c', 'diff', 'gitcommit', 'git_rebase', 'html', 'json', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'toml', 'vim', 'vimdoc', 'go' }
       require('nvim-treesitter').install(parsers)
       vim.api.nvim_create_autocmd('FileType', {
         callback = function(args)
